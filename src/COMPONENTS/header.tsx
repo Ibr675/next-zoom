6770d6d37786c9b6babdf1a9e0ad0c9ece538d1f
irahim <ibrahimcheema424@gmail.com>
2024-09-15 04:55:00 -0700

    VVV

--- src/COMPONENTS/header.tsx
+++ src/COMPONENTS/header.tsx
@@ -3,9 +3,9 @@ import LINK from 'next/link'
 const Header = () => {
   return (
     <div className='flex gap-10 p-10 bg-red-400'> 
-      <LINK href="\"> INTRIDUCTION PAGE </LINK>
-      <LINK href="\HOME"> HOME PAGE </LINK>
-      <LINK href="\CONTACT"> CONTACT PAGE </LINK>
+      <LINK href="\"> LIKE </LINK>
+      <LINK href="\HOME"> SHEAR </LINK>
+      <LINK href="\CONTACT"> SUBSCRIBE </LINK>
     </div>
   )
 }

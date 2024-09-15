617a1321e7e974264d2da0427c09ca4f534755c6
irahim <ibrahimcheema424@gmail.com>
2024-09-15 10:03:03 -0700

    ,,

--- src/COMPONENTS/footer.tsx
+++ src/COMPONENTS/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import LINK from 'next/link'
 const footer = () => {
   return (
-    <div className='flex gap-8 p-6 bg-red-400'> 
+    <div className='flex gap-6 p-6 bg-red-400'> 
       <LINK href="\"> OUR   </LINK>
       <LINK href="\HOME"> JOURNEY </LINK>
       <LINK href="\CONTACT"> BEGINS </LINK>

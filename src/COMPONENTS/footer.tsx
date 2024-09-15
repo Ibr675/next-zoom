7f1213272bbce33f4823d37b51fc04a0da64cdc5
irahim <ibrahimcheema424@gmail.com>
2024-09-15 04:51:25 -0700

    JJ

--- src/COMPONENTS/footer.tsx
+++ src/COMPONENTS/footer.tsx
@@ -3,9 +3,9 @@ import LINK from 'next/link'
 const footer = () => {
   return (
     <div className='flex gap-8 p-6 bg-red-400'> 
-      <LINK href="\">INTRIDUCTION PAGE</LINK>
-      <LINK href="\HOME">HOME PAGE</LINK>
-      <LINK href="\CONTACT">CONTACT PAGE</LINK>
+      <LINK href="\"> INTRODUCTION PAGE </LINK>
+      <LINK href="\HOME"> HOME PAGE </LINK>
+      <LINK href="\CONTACT"> CONTACT PAGE </LINK>
     </div>
   )
 }
--- src/COMPONENTS/header.tsx
+++ src/COMPONENTS/header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import LINK from 'next/link'
 const Header = () => {
   return (
-    <div className='flex gap-8 p-6 bg-red-400'> 
+    <div className='flex gap-10 p-10 bg-red-400'> 
       <LINK href="\"> INTRIDUCTION PAGE </LINK>
       <LINK href="\HOME"> HOME PAGE </LINK>
       <LINK href="\CONTACT"> CONTACT PAGE </LINK>

4143bb371d66fd5016acc1166e3dd6c58820457c
irahim <ibrahimcheema424@gmail.com>
2024-09-15 09:53:02 -0700

    mmk

--- src/COMPONENTS/header.tsx
+++ src/COMPONENTS/header.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import LINK from 'next/link'
 const Header = () => {
   return (
-    <div className='flex gap-10 p-10 bg-red-400'> 
+    <div className='flex gap-5 p-5 bg-red-400'> 
       <LINK href="\"> INTRODUCTION </LINK>
       <LINK href="\HOME"> HOME</LINK>
       <LINK href="\CONTACT"> CONTACT </LINK>

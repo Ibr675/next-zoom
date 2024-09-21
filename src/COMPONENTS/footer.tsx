af936b086578b0e6b3f0b769aaa6767809cc8f82
irahim <ibrahimcheema424@gmail.com>
2024-09-21 05:34:01 -0700

    FINAL

--- src/COMPONENTS/footer.tsx
+++ src/COMPONENTS/footer.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import LINK from 'next/link'
-const footer = () => {
-  return (
-    <div className='flex gap-6 p-6 bg-red-400'> 
-      <LINK href="\"> OUR   </LINK>
-      <LINK href="\HOME"> JOURNEY </LINK>
-      <LINK href="\CONTACT"> BEGINS </LINK>
-      <LINK href="\BUSNESS"> HEAR </LINK>
-    </div>
-  )
-}
+import React from 'react';
+import Link from 'next/link';
 
-export default footer
+const Footer = () => {
+  return (
+    <div className="flex justify-center items-center p-6 bg-gradient-to-r from-purple-500 to-pink-500 text-white">
+      <div className="flex gap-6">
+        <Link href="/" className="footer-link">
+          OUR STORY
+        </Link>
+        <Link href="/HOME" className="footer-link">
+          OUR HOUSE
+        </Link>
+        <Link href="/CONTACT" className="footer-link">
+          GET IN TOUCH
+        </Link>
+        <Link href="/BUSNESS" className="footer-link">
+          OUR BUSINESS
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- src/COMPONENTS/header.tsx
+++ src/COMPONENTS/header.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import LINK from 'next/link'
+import React from 'react';
+import Link from 'next/link';
+
 const Header = () => {
   return (
-    <div className='flex gap-5 p-5 bg-red-400'> 
-      <LINK href="\"> INTRODUCTION </LINK>
-      <LINK href="\HOME"> HOME</LINK>
-      <LINK href="\CONTACT"> CONTACT </LINK>
-      <LINK href="\BUSNESS"> BUSNESS </LINK>
+    <div className="flex justify-center items-center p-6 bg-gradient-to-r from-purple-500 to-pink-500 text-white">
+      <div className="flex gap-6">
+        <Link href="/" className="text-lg font-bold text-white hover:text-gray-200">
+          OUR STORY
+        </Link>
+        <Link href="/HOME" className="text-lg font-bold text-white hover:text-gray-200">
+          OUR HOME
+        </Link>
+        <Link href="/CONTACT" className="text-lg font-bold text-white hover:text-gray-200">
+          GET IN TOUCH
+        </Link>
+        <Link href="/BUSNESS" className="text-lg font-bold text-white hover:text-gray-200">
+         OUR BUSNESS
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
